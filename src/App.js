2113e678569cd7eac54a98045d0935f3a3d13c8f
ejoubaud <emmanuel.joubaud@gmail.com>
2020-04-16 22:54:45 +0200

    Add deck-of-cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import 'deck-of-cards/example/example.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="container">
     </div>
   );
 }
