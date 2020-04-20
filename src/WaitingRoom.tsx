6d216c2a22a2330fe7359342c034799006532494
ejoubaud <emmanuel.joubaud@gmail.com>
2020-04-20 10:45:44 +0200

    Tweak styles

--- src/WaitingRoom.tsx
+++ src/WaitingRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, FormEvent, MouseEvent } from "react";
+import React, { useRef, FormEvent } from "react";
 
 import { PlayerId } from "./store";
 import "./WaitingRoom.css";
@@ -18,19 +18,22 @@ const WaitingRoom = ({
   const url = `${window.location.host}/join/${roomId}`;
   const urlRef = useRef<HTMLPreElement>(null);
 
-  const selectUrl = (evt: MouseEvent<HTMLPreElement>) => {
+  const selectUrl = () => {
     const selection = window.getSelection();
     if (!selection) return;
     if (urlRef && urlRef.current) selection.selectAllChildren(urlRef.current);
   };
 
+  const unselectUrl = () => {
+    const selection = window.getSelection();
+    if (!selection || !selection.empty) return;
+    selection.empty();
+  }
+
   const copyUrl = () => {
     const { clipboard } = navigator;
     if (!clipboard || !clipboard.writeText) return;
     clipboard.writeText(url);
-    const selection = window.getSelection();
-    if (!selection || !selection.empty) return;
-    selection.empty();
   };
 
   return (
@@ -41,10 +44,13 @@ const WaitingRoom = ({
           <p>
             Envoyez cette URL aux autres joueurs pour qu'ils vous rejoignent:
           </p>
-          <pre onClick={selectUrl} ref={urlRef}>
+          <code onClick={selectUrl} ref={urlRef}>
             {url}
-          </pre>
-          <input type="button" onClick={copyUrl} value="Copier l'URL" />
+          </code>
+          <button className="WaitingRoom__CopyUrlBtn btn-ghost" onClick={copyUrl} onMouseDown={selectUrl} onMouseUp={unselectUrl} onMouseOut={unselectUrl}>
+            <i className="icon-bag" />
+            Copier l'URL
+          </button>
         </div>
       ) : (
         <form onSubmit={onNameSubmit}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "simple-line-icons/css/simple-line-icons.css";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
